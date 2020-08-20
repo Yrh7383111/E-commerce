171d94f0a320d29e6803f5fad4f47d42bfb7f38b
Jackson Yin <Yrh7383111@outlook.com>
2020-08-20 10:51:16 -0400

    Clean up

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -15,7 +15,6 @@ const CollectionPreview = ({ title, items }) => (
             {items.filter((item, index) => index < 4)
                 // Object destructing
                 .map(item => (
-                    // ...otherProps - remaining properties in item with the same name passed through
                     <CollectionItem key={item.id} item={item} />
                 ))}
         </div>
