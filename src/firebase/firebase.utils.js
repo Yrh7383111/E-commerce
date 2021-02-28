595641f415e5605cea686fdec27d376d8233aa01
Jackson Yin <Yrh7383111@outlook.com>
2021-02-28 21:35:56 +0800

    Clean up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,14 +6,14 @@ import 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDz6ujTDViXPrpNZFPq0V1hxm-1_jan2Ak",
-    authDomain: "e-commerce-7b83e.firebaseapp.com",
-    databaseURL: "https://e-commerce-7b83e.firebaseio.com",
-    projectId: "e-commerce-7b83e",
-    storageBucket: "e-commerce-7b83e.appspot.com",
-    messagingSenderId: "718043204346",
-    appId: "1:718043204346:web:2321110cf5f0d0e890d743",
-    measurementId: "G-XFPC7B9SH2"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
