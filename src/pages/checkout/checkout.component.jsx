abba6c0bf224d0cbde294f1c94729bf578ca6792
Jackson Yin <jackson.yin@veeva.com>
2022-01-30 18:54:38 -0500

    Update useSelector for Checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartItemsTotal } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
@@ -13,56 +13,55 @@ import {
 
 
 
-const CheckoutPage = ({ cartItems, total }) => (
-    <CheckoutPageContainer>
-        <CheckoutHeaderContainer>
-            <HeaderBlockContainer>
-                <span>Product</span>
-            </HeaderBlockContainer>
-            <HeaderBlockContainer>
-                <span>Description</span>
-            </HeaderBlockContainer>
-            <HeaderBlockContainer>
-                <span>Quantity</span>
-            </HeaderBlockContainer>
-            <HeaderBlockContainer>
-                <span>Price</span>
-            </HeaderBlockContainer>
-            <HeaderBlockContainer>
-                <span>Clear</span>
-            </HeaderBlockContainer>
-        </CheckoutHeaderContainer>
-
-        {/* Loop through cartItems */}
-        {cartItems.map(cartItem => (
-            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-
-        <TotalContainer className='total'>
-            TOTAL: ${total}
-        </TotalContainer>
-
-        <WarningContainer>
-            *Please use the following test credit card for payments*
-            <br />
-            4242 4242 4242 4242 - Exp: 07/23 - CVV: 123
-        </WarningContainer>
-
-        <StripeCheckoutButton price={total} />
-    </CheckoutPageContainer>
-);
-
-
-// Retrieve props from store
-const mapStateToProps = createStructuredSelector({
+const CheckoutPage = () => {
+    // Retrieve props from store
     // Caching - Memoization on Header component
 
     // If state.cart doesn't change, memoize cartItems
-    cartItems: state => selectCartItems(state),
+    const cartItems = useSelector(selectCartItems);
     // If state.cart doesn't change, memoize total
-    total: state => selectCartItemsTotal(state)
-})
+    const total = useSelector(selectCartItemsTotal);
 
 
+    return (
+        <CheckoutPageContainer>
+            <CheckoutHeaderContainer>
+                <HeaderBlockContainer>
+                    <span>Product</span>
+                </HeaderBlockContainer>
+                <HeaderBlockContainer>
+                    <span>Description</span>
+                </HeaderBlockContainer>
+                <HeaderBlockContainer>
+                    <span>Quantity</span>
+                </HeaderBlockContainer>
+                <HeaderBlockContainer>
+                    <span>Price</span>
+                </HeaderBlockContainer>
+                <HeaderBlockContainer>
+                    <span>Clear</span>
+                </HeaderBlockContainer>
+            </CheckoutHeaderContainer>
 
-export default connect(mapStateToProps)(CheckoutPage);
+            {/* Loop through cartItems */}
+            {cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+
+            <TotalContainer className='total'>
+                TOTAL: ${total}
+            </TotalContainer>
+
+            <WarningContainer>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 07/23 - CVV: 123
+            </WarningContainer>
+
+            <StripeCheckoutButton price={total} />
+        </CheckoutPageContainer>
+    );
+};
+
+
+export default CheckoutPage;
