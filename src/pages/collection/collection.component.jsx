610bc3fc163e4b4edb01be505eae716b543b4b6d
Jackson Yin <jackson.yin@veeva.com>
2022-01-30 18:50:47 -0500

    Update useSelector and useParams for Collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useParams } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import { CollectionPageContainer, ItemsContainer, TitleContainer } from "./collection.styles";
 
 
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = () => {
+    const collectionId = useParams();
+    // Retrieve props from store
+    // ownProps = this.props
+    const collection = useSelector(selectCollection(collectionId));
     const { title, items } = collection;
 
     return (
@@ -21,12 +26,4 @@ const CollectionPage = ({ collection }) => {
 )};
 
 
-// Retrieve props from store
-// ownProps = this.props
-const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
