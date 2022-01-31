65e897a814c23633ea974dc920e301486558ba17
Jackson Yin <jackson.yin@veeva.com>
2022-01-30 19:32:21 -0500

    Update useSelector for Directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import MenuItem from "../menu-item/menu-item.component";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
@@ -8,26 +8,24 @@ import { DirectoryMenuContainer } from "./directory.styles";
 
 
 // Directory component that contains all the Menu Items
-const Directory = ({ sections }) => (
-    <DirectoryMenuContainer>
-        {/* Destructing */}
-        {/* section -> { id, title, imageUrl, size, linkUrl } */}
-        {/* ...otherProps - remaining properties in section with the same name passed through */}
-        {sections.map(({ id, ...otherProps }) => (
-            <MenuItem key={id} {...otherProps} />
-        ))}
-    </DirectoryMenuContainer>
-);
-
-
-// Retrieve props from store
-const mapStateToProps = createStructuredSelector({
+const Directory = () => {
+    // Retrieve props from store
     // Caching - Memoization on Directory component
-
     // If state.directory doesn't change, memoize sections
-    sections: state => selectDirectorySections(state)
-});
+    const sections = useSelector(selectDirectorySections);
 
 
+    return (
+        <DirectoryMenuContainer>
+            {/* Destructing */}
+            {/* section -> { id, title, imageUrl, size, linkUrl } */}
+            {/* ...otherProps - remaining properties in section with the same name passed through */}
+            {sections.map(({ id, ...otherProps }) => (
+                <MenuItem key={id} {...otherProps} />
+            ))}
+        </DirectoryMenuContainer>
+    );
+};
 
-export default connect(mapStateToProps)(Directory);
+
+export default Directory;
