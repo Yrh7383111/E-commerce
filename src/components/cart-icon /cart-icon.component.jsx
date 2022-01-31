2253dc051cfdd5e370004589bef9db47b999dcac
Jackson Yin <jackson.yin@veeva.com>
2022-01-30 19:13:37 -0500

    Update useSelector and useDispatchfor Cart Icon

--- src/components/cart-icon /cart-icon.component.jsx
+++ src/components/cart-icon /cart-icon.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
@@ -10,28 +10,20 @@ import { CartIconContainer, ItemCountContainer, ShoppingIconContainer } from "./
 // Cart Icon component
 // Object destructing
 // this.props -> { toggleCartHidden, itemCount }
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-    <CartIconContainer onClick={toggleCartHidden}>
-        <ShoppingIconContainer />
-        <ItemCountContainer>{itemCount}</ItemCountContainer>
-    </CartIconContainer>
-);
-
-
-// Retrieve props from store
-const mapStateToProps = createStructuredSelector({
+const CartIcon = () => {
+    // Retrieve props from store
     // Caching - Memoization on Cart Icon component
-
     // If state.cart doesn't change, memoize itemCount
-    itemCount: state => selectCartItemsCount(state)
-});
+    const itemCount = useSelector(selectCartItemsCount);
+    const dispatch = useDispatch();
 
-// Dispatch Actions to all Reducers
-const mapDispatchToProps = dispatch => ({
-    // dispatch - packs up the argument as an Action object, and deliveries it to all Reducers
-    toggleCartHidden: () => dispatch(toggleCartHidden())
-});
+    return (
+        <CartIconContainer onClick={() => dispatch(toggleCartHidden())}>
+            <ShoppingIconContainer />
+            <ItemCountContainer>{itemCount}</ItemCountContainer>
+        </CartIconContainer>
+    );
+};
 
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+export default CartIcon;
